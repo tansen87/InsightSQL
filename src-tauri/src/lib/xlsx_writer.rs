ed2b598d2b120767b9ee6e33803f20297aa3b019
kimura <tansen87@qq.com>
2024-11-04 21:36:50 +0800

    fix(xlsx_writer): ensure that each Series in the DataFrame is a single chunk

--- src-tauri/src/lib/xlsx_writer.rs
+++ src-tauri/src/lib/xlsx_writer.rs
@@ -1,20 +1,36 @@
 use std::{error::Error, path::PathBuf};
 
-use polars::{datatypes::AnyValue, frame::DataFrame};
+use polars::{datatypes::AnyValue, frame::DataFrame, series::Series};
+
+fn rechunk_series(series: &Series) -> Series {
+  if series.chunks().len() > 1 {
+    series.rechunk()
+  } else {
+    series.clone()
+  }
+}
 
 pub fn write_xlsx(df: DataFrame, output_path: PathBuf) -> Result<(), Box<dyn Error>> {
   /* write dataframe to xlsx */
   let mut workbook = rust_xlsxwriter::Workbook::new();
   let worksheet = workbook.add_worksheet();
 
+  // Ensure that each Series in the DataFrame is a single chunk
+  let rechunk_df = df
+    .iter()
+    .map(|series| rechunk_series(series))
+    .collect::<Vec<_>>()
+    .into_iter()
+    .collect::<DataFrame>();
+
   // write headers to xlsx
-  let headers = df.get_column_names();
+  let headers = rechunk_df.get_column_names();
   for (col, col_name) in headers.iter().enumerate() {
     worksheet.write_string(0, col.try_into()?, col_name.to_string())?;
   }
 
   // write data to xlsx
-  for (row, row_data) in df.iter().enumerate() {
+  for (row, row_data) in rechunk_df.iter().enumerate() {
     for (col, col_data) in row_data.iter().enumerate() {
       match col_data {
         AnyValue::Float64(values) => {
