4dec1cc395197e90cdc5ee5f44f35391b320397a
kimura <tansen87@qq.com>
2025-01-11 10:24:02 +0800

    fix(excel_reader): iterating and writing headers or duplicate headers

--- src-tauri/src/lib/excel_reader.rs
+++ src-tauri/src/lib/excel_reader.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, fs::File, io::BufReader, path::Path};
+use std::{collections::HashMap, fmt::Display, fs::File, io::BufReader, path::Path};
 
 use anyhow::{anyhow, Result};
 use calamine::{CellType, Data, DataType, HeaderRow, Range, Reader};
@@ -19,14 +19,25 @@ where
   fn to_df(&mut self) -> Result<DataFrame> {
     let mut columns = Vec::new();
 
-    // Headers
-    let headers: Vec<String> = self
-      .rows()
-      .next()
-      .ok_or(anyhow!("No data"))?
-      .iter()
-      .map(|cell| cell.to_string())
-      .collect();
+    // iterating and writing headers or duplicate headers
+    let mut header_counts = HashMap::<String, usize>::new();
+    let headers: Vec<String> = match self.rows().next() {
+      Some(first_row) => first_row
+        .iter()
+        .map(|cell| {
+          let cell_str = cell.to_string();
+          let count = header_counts.entry(cell_str.clone()).or_insert(0);
+          let current_count = *count;
+          *count += 1;
+          if current_count > 0 {
+            format!("{}_duplicated_{}", cell_str, current_count - 1)
+          } else {
+            cell_str
+          }
+        })
+        .collect(),
+      None => return Err(anyhow!("No data")),
+    };
 
     // Vec<String> for each column
     for _ in 0..headers.len() {
@@ -71,7 +82,11 @@ impl ExcelReader {
   /// Get the nth worksheet. Shortcut for getting the nth
   /// sheet_name, then the corresponding worksheet.
   pub fn worksheet_range_at(&mut self, n: usize, skip_rows: u32) -> Result<Range<Data>> {
-    match self.workbook.with_header_row(HeaderRow::Row(skip_rows)).worksheet_range_at(n) {
+    match self
+      .workbook
+      .with_header_row(HeaderRow::Row(skip_rows))
+      .worksheet_range_at(n)
+    {
       Some(Ok(sheet_range)) => Ok(sheet_range),
       Some(Err(e)) => Err(e.into()),
       None => Err(anyhow!("Worksheet index out of bounds")),
