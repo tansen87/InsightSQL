333405b6210bce586741450e5fdfffe49a45ebb6
kimura <tansen87@qq.com>
2025-08-03 10:00:14 +0800

    test(transpose): add tests to transpose

--- src-tauri/src/lib/cmd/transpose.rs
+++ src-tauri/src/lib/cmd/transpose.rs
@@ -7,6 +7,7 @@ use std::{
 
 use anyhow::Result;
 use csv::{ByteRecord, ReaderBuilder, WriterBuilder};
+use memmap2::MmapOptions;
 
 use crate::io::csv::options::CsvOptions;
 
@@ -17,22 +18,30 @@ pub async fn in_memory_transpose<P: AsRef<Path> + Send + Sync>(path: P) -> Resul
   let mut output_path = PathBuf::from(csv_options.parent_path()?);
   output_path.push(format!("{file_stem}.transpose.csv"));
 
-  let mut rdr = ReaderBuilder::new()
+  let nrows = ReaderBuilder::new()
     .delimiter(sep)
-    .from_reader(csv_options.rdr_skip_rows()?);
+    .from_reader(csv_options.rdr_skip_rows()?)
+    .byte_headers()?
+    .len();
 
+  let file = File::open(path)?;
+  let mmap = unsafe { MmapOptions::new().populate().map(&file)? };
   let mut wtr = WriterBuilder::new()
     .delimiter(sep)
     .from_writer(BufWriter::new(File::create(output_path)?));
 
-  let nrows = rdr.byte_headers()?.len();
+  let mut record = ByteRecord::with_capacity(1024, nrows);
 
-  let all = rdr.byte_records().collect::<Result<Vec<_>, _>>()?;
   for i in 0..nrows {
-    let mut record = ByteRecord::new();
+    record.clear();
 
-    for row in &all {
-      record.push_field(&row[i]);
+    let mut rdr = ReaderBuilder::new().delimiter(sep).from_reader(&mmap[..]);
+
+    for row in rdr.byte_records() {
+      let row = row?;
+      if i < row.len() {
+        record.push_field(&row[i]);
+      }
     }
     wtr.write_byte_record(&record)?;
   }
@@ -57,7 +66,10 @@ pub async fn multipass_transpose<P: AsRef<Path> + Send + Sync>(path: P) -> Resul
 
   let nrows = rdr.byte_headers()?.len();
 
+  let mut record = ByteRecord::with_capacity(1024, nrows);
+
   for i in 0..nrows {
+    record.clear();
     let mut rdr = ReaderBuilder::new()
       .delimiter(sep)
       .from_reader(File::open(&path)?);
---
+++ src-tauri/tests/test_transpose.rs
@@ -0,0 +1,52 @@
+fn create_temp_csv() -> anyhow::Result<(tempfile::TempDir, String, String)> {
+  let temp_dir = tempfile::TempDir::new()?;
+
+  let data = vec![
+    "name,age,gender",
+    "Tom,18,female",
+    "Jerry,19,male",
+    "Patrick,4,male",
+  ];
+  let file_path = temp_dir.path().join("input.csv");
+  let mut wtr = csv::Writer::from_path(&file_path)?;
+  for line in &data {
+    wtr.write_record(line.split(',').map(|s| s.as_bytes()))?;
+  }
+  wtr.flush()?;
+
+  let path = file_path.to_string_lossy().to_string();
+  let output_path = temp_dir
+    .path()
+    .join("input.transpose.csv")
+    .to_str()
+    .unwrap()
+    .to_string();
+
+  Ok((temp_dir, path, output_path))
+}
+
+#[tokio::test]
+async fn test_memory() -> anyhow::Result<()> {
+  let (temp_dir, path, output_path) = create_temp_csv()?;
+
+  lib::cmd::transpose::in_memory_transpose(path).await?;
+  let context = std::fs::read_to_string(output_path)?;
+  let result = context.trim().split('\n').collect::<Vec<_>>();
+  let expected = vec!["Tom,Jerry,Patrick", "18,19,4", "female,male,male"];
+  assert_eq!(expected, result);
+
+  Ok(temp_dir.close()?)
+}
+
+#[tokio::test]
+async fn test_multipass() -> anyhow::Result<()> {
+  let (temp_dir, path, output_path) = create_temp_csv()?;
+
+  lib::cmd::transpose::multipass_transpose(path).await?;
+  let context = std::fs::read_to_string(output_path)?;
+  let result = context.trim().split('\n').collect::<Vec<_>>();
+  let expected = vec!["Tom,Jerry,Patrick", "18,19,4", "female,male,male"];
+  assert_eq!(expected, result);
+
+  Ok(temp_dir.close()?)
+}
